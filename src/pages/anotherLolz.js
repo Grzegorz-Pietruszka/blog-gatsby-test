4c9bb84c32ad287da1c077f19fa714d0126a556b
GrzegorzP <pietruszka.grzegorz@gmail.com>
2020-10-05 10:59:46 +0100

    initail commit all

---
+++ src/pages/anotherLolz.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const AnotherLolz = () => {
+  return (
+    <Layout>
+      <div>
+        Polska to kurwa jebana
+      </div>
+    </Layout>
+  )
+}
+
+export default AnotherLolz
---
+++ src/pages/lol.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Lol = () => (
+    <Layout>
+      <SEO title={'lol'} />
+      <div>
+        Sasin rozpierdolił 70 mln publicznych pieniędzy i za to nie odpowiedział
+      </div>
+    </Layout>
+)
+
+export default Lol
