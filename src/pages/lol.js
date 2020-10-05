2aa2c8af9cbba1af10bfd6054d9182af50b474d1
GrzegorzP <pietruszka.grzegorz@gmail.com>
2020-10-05 12:38:59 +0100

    added links, added activeStyle attribute

--- src/pages/lol.js
+++ src/pages/lol.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const Lol = () => (
     <Layout>
@@ -8,6 +9,12 @@ const Lol = () => (
       <div>
         Sasin rozpierdolił 70 mln publicznych pieniędzy i za to nie odpowiedział
       </div>
+
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link
+        activeStyle={{color: 'red'}}
+        to="/lol/">go for lolz</Link> <br />
+      <Link to="/anotherLolz/">another lolz</Link>
     </Layout>
 )
 
